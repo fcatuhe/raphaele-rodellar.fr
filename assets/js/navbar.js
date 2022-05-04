426bf347b94f42c2b933873f7a3df7da5b793cdc
François Catuhe <francois@hey.com>
2022-05-04 23:47:06 +0200

    Lint with prettier

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,67 +1,59 @@
 $(document).ready(function () {
-
   //////////////// menu sticky
-  var nav = $('nav');
-  var hauteurMenu = nav.height();
-
+  var nav = $('nav')
+  var hauteurMenu = nav.height()
 
   //comportement du menu en mobil :rendre opaque le site quand on ouvre le menu en mobil
   // affichage de la flèche scroll bottom de la bannière top de la home
   // quand le menu n'est pas déployé
   $('.navbar-burger').on('click', function (e) {
-    $('#burger').toggleClass('open');
+    $('#burger').toggleClass('open')
     if ($('.navbar-menu').hasClass('show')) {
-      closeMenu();
+      closeMenu()
     } else {
-      openMenu();
+      openMenu()
     }
-  });
+  })
 
   //ok condition open and close
   $('.nav-link').on('click', function (e) {
     if ($('.navbar-menu').hasClass('show')) {
-      closeMenu();
+      closeMenu()
     } else {
-      openMenu();
+      openMenu()
     }
-  });
+  })
 
   function openMenu() {
-    $('.navbar-menu').addClass('show');
-    $('.dark-backdrop').addClass('in');
-
+    $('.navbar-menu').addClass('show')
+    $('.dark-backdrop').addClass('in')
   }
 
   function closeMenu() {
-    $('.navbar-menu').removeClass('show');
-    $('.dark-backdrop').removeClass('in');
-
+    $('.navbar-menu').removeClass('show')
+    $('.dark-backdrop').removeClass('in')
   }
 
-  $(window).on("resize", function (e) {
+  $(window).on('resize', function (e) {
     if (window.innerWidth > 992) {
       if ($('.navbar-menu').hasClass('show')) {
-        closeMenu();
-        $('.navbar-menu').removeClass('show');
+        closeMenu()
+        $('.navbar-menu').removeClass('show')
       }
     }
-  });
+  })
 
   $(window).scroll(function () {
     //scrollTop()=> retourne la position de la barre de défilement de l'element
     //au démarage=0
-    var scroll = $(this).scrollTop();
+    var scroll = $(this).scrollTop()
     //si la position de la barre de defilement de windowsest >= à la position haute du header
     if (scroll >= hauteurMenu) {
-      nav.addClass('sticked');
+      nav.addClass('sticked')
     } else {
-      nav.removeClass('sticked');
+      nav.removeClass('sticked')
     }
-  });
+  })
 
-
-
-
-  $(window).trigger('scroll');
-
-});
+  $(window).trigger('scroll')
+})

bddd0006425cdd9897935370cdbdbfe00d7ea1ee
François Catuhe <francois@hey.com>
2023-07-16 19:03:38 +0200

    Fix image tags and root URL

--- helpers/url_helpers.rb
+++ helpers/url_helpers.rb
@@ -7,7 +7,16 @@ module UrlHelpers
     [base_url, path].join
   end
 
-  alias_method :site_url, :absolute_url
+  alias site_url absolute_url
+
+  def root_url_or_scroll_to_top(current_page_url)
+    if current_page_url == '/'
+      site_url('#')
+    else
+      site_url
+    end
+
+  end
 
   def image_url(image_filename)
     absolute_url image_path(image_filename)

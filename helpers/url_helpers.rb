c5fa78963288831e32ac9c4af0d1e725ce5e3548
François Catuhe <francois@ridecell.com>
2024-12-27 12:57:52 +0100

    Improve SEO

--- helpers/url_helpers.rb
+++ helpers/url_helpers.rb
@@ -4,10 +4,14 @@ module UrlHelpers
   end
 
   def absolute_url(path = nil)
-    [ base_url, path ].join
+    [ base_url, path ].join.delete_suffix('.html').delete_suffix('/')
   end
 
   alias site_url absolute_url
+
+  def current_page_canonical_url
+    site_url(current_page.url)
+  end
 
   def root_url_or_scroll_to_top(current_page_url)
     current_page_url == "/" ? site_url("#") : site_url

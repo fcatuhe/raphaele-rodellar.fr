8a934c23e08fd36d8a4718c9dd8d10353f518fe8
François Catuhe <francois@hey.com>
2025-12-02 14:28:06 +0100

    Replace og_meta_tags partial with helper method
    Use Rails-idiomatic pattern (.join("\n").html_safe) matching
    csrf_meta_tags implementation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,18 +10,22 @@ module ApplicationHelper
   end
 
   def canonical_url
-    url_for(only_path: false)
+    url_for(only_path: false).delete_suffix("/")
   end
 
-  def og_meta_tag(key, content)
-    case key
-    when :title, :description, :image
-      tag(:meta, name: key, property: "og:#{key}", content: content)
-    when :author
-      tag(:meta, name: key, content: content)
-    else
-      tag(:meta, property: "og:#{key}", content: content)
-    end
+  def og_meta_tags(**tags)
+    tags.filter_map do |key, content|
+      next if content.blank?
+
+      case key
+      when :title, :description, :image
+        tag.meta(name: key, property: "og:#{key}", content: content)
+      when :author
+        tag.meta(name: key, content: content)
+      else
+        tag.meta(property: "og:#{key}", content: content)
+      end
+    end.join("\n").html_safe
   end
 
   def root_path_or_scroll_to_top

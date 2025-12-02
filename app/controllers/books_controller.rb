d65ce63d6a36eaca02c2c9c6925449c48bfa0bdf
François Catuhe <francois@hey.com>
2025-12-02 11:45:42 +0100

    Bring back lost behavior temporarily

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,14 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:slug])
+
+    @page = OpenStruct.new(
+      title: @book.title,
+      description: @book.subtitle.presence,
+      layout: "book",
+      body_class: "book",
+      slug: @book.slug
+    )
   rescue Decant::FileNotFound
     render_not_found
   end

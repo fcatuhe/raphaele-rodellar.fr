fea3620ff401a1931ef948298131c27a16bdadfa
François Catuhe <francois@hey.com>
2025-12-02 13:56:42 +0100

    Use Meta.disallow_all? in Parkfile for sitemap generation
    Centralize staging/production logic in Meta class instead of
    duplicating it in Parkfile.

--- app/models/meta.rb
+++ app/models/meta.rb
@@ -1,16 +1,16 @@
 class Meta < ActiveSupport::CurrentAttributes
   SITE_TITLE = "Raphaële Rodellar Thérapeute Réflexologue Bordeaux".freeze
-  SITE_NAME = "Raphaële Rodellar".freeze
-  AUTHOR = "Raphaële Rodellar".freeze
   DEFAULT_DESCRIPTION = "Se mettre à l'écoute de son corps, se relâcher, se relaxer, se recharger, ... Venez profiter de tous les bienfaits de la réflexologie plantaire !".freeze
   DEFAULT_IMAGE = "og-image.jpg".freeze
+  AUTHOR = "Raphaële Rodellar".freeze
+  SITE_NAME = "Raphaële Rodellar".freeze
   DEFAULT_TYPE = "website".freeze
 
   attribute :layout, :body_class
   attr_writer :title, :description, :image, :type
 
   def title
-    [@title, SITE_TITLE].compact_blank.join(" · ")
+    [ @title, SITE_TITLE ].compact_blank.join(" · ")
   end
 
   def description
@@ -21,16 +21,16 @@ class Meta < ActiveSupport::CurrentAttributes
     @image.presence || DEFAULT_IMAGE
   end
 
+  def author
+    AUTHOR
+  end
+
   def type
     @type.presence || DEFAULT_TYPE
   end
 
   def site_name
     SITE_NAME
-  end
-
-  def author
-    AUTHOR
   end
 
   def robots_content
@@ -52,13 +52,16 @@ class Meta < ActiveSupport::CurrentAttributes
     %w[curious book].include?(layout)
   end
 
+  def disallow_all?
+    !production_live?
+  end
+
   private
+    def production_live?
+      Rails.env.production? && !staging?
+    end
 
-  def staging?
-    ActiveModel::Type::Boolean.new.cast(ENV["STAGING"])
-  end
-
-  def production_live?
-    Rails.env.production? && !staging?
-  end
+    def staging?
+      ActiveModel::Type::Boolean.new.cast(ENV["STAGING"])
+    end
 end
